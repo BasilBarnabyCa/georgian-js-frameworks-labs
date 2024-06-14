79694b4a6995d95397b933beaac036fb06e66b8e
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-06-14 17:41:56 -0400

    Added contact page

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -10,8 +10,8 @@ router.get('/portfolio', function(req, res, next) {
 	res.render('portfolio', { title: 'It\'s Basil | Portfolio' });
   });
 
-router.get('/experience', function(req, res, next) {
-	res.render('experience', { title: 'It\'s Basil | Experience' });
+router.get('/contact', function(req, res, next) {
+	res.render('contact', { title: 'It\'s Basil | Contact' });
   });
 
 module.exports = router;
