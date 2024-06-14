74eb4fede7c424ca4ef918cbf830dd4794889971
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-06-14 19:24:12 -0400

    Added experience page

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -1,17 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'It\'s Basil | Home' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "It's Basil | Home" });
 });
 
-router.get('/portfolio', function(req, res, next) {
-	res.render('portfolio', { title: 'It\'s Basil | Portfolio' });
-  });
+router.get("/portfolio", function (req, res, next) {
+  res.render("portfolio", { title: "It's Basil | Portfolio" });
+});
 
-router.get('/contact', function(req, res, next) {
-	res.render('contact', { title: 'It\'s Basil | Contact' });
-  });
+router.get("/experience", function (req, res, next) {
+  res.render("experience", { title: "It's Basil | Experience" });
+});
+
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "It's Basil | Contact" });
+});
 
 module.exports = router;
