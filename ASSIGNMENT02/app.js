cad028524e0f15bd3d2361fa6b4011adf06f2ea3
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-08-06 17:41:05 -0400

    Added mongoose integration

--- ASSIGNMENT02/app.js
+++ ASSIGNMENT02/app.js
@@ -3,9 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var config = require('./config/globals');
+var mongoose = require('mongoose');
 
 var adminRouter = require('./routes/airportAdmin');
 var usersRouter = require('./routes/users');
+
+// Connect to MongoDB
+mongoose
+	.connect(config.ConnectionStrings.MongoDB, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then((message) => console.log("Connected Successfully!"))
+	.catch((error) => console.log(`Error while connecting: ${error}`));
 
 var app = express();
 
