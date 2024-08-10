e503c7a4d20a6e2980f1133d4d97fead8c250711
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-08-09 21:45:00 -0400

    Added collections demo page

--- ASSIGNMENT02/routes/guestRoutes/guest.js
+++ ASSIGNMENT02/routes/guestRoutes/guest.js
@@ -4,9 +4,22 @@ var guestLayout = "layouts/guest";
 var passport = require("passport");
 const user = require("../../models/user");
 
+const Airline = require("../../models/airline");
+
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Home", layout: guestLayout, user: req.user });
+});
+
+/* GET /airlines-demo */
+router.get("/airlines-demo", async (req, res, next) => {
+	try {
+		let airlines = await Airline.find().sort([["name", "ascending"]]);
+		res.render("airlines-demo", { layout: guestLayout, title: "Airlines Collection Demo", dataset: airlines });
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = router;
